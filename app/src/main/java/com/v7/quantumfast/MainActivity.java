43b8b4193284bf23e7543506cfa2feb326be84dd
thibautfihey49-hue <thibautfihey49-hue@users.noreply.github.com>
2026-08-18 18:19:27 +0200

    Fix crash AppCompat theme - MainActivity extends Activity

--- app/src/main/java/com/v7/quantumfast/MainActivity.java
+++ app/src/main/java/com/v7/quantumfast/MainActivity.java
@@ -1,14 +1,9 @@
 package com.v7.quantumfast;
+import android.app.Activity;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager2.widget.ViewPager2;
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     @Override protected void onCreate(Bundle b){
         super.onCreate(b);
         setContentView(R.layout.activity_main);
-        ViewPager2 vp=findViewById(R.id.viewPager);
-        vp.setAdapter(new PagesAdapter(this));
-        vp.setCurrentItem(0,false); // Page3 = principale
-        vp.setOffscreenPageLimit(3);
     }
 }
